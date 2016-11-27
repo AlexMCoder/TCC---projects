c0a23eeef48ba4812f80d721208a39e138d95b49
AlexMCoder <alexmpprog@gmail.com>
2016-11-27 09:58:57 -0200

    implementação da pesquisa de produtos concluido

--- PedidoVenda/src/main/java/com/projeto/pedidovenda/controller/PesquisaProdutosBean.java
+++ PedidoVenda/src/main/java/com/projeto/pedidovenda/controller/PesquisaProdutosBean.java
@@ -5,25 +5,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import com.projeto.pedidovenda.model.Produto;
+import com.projeto.pedidovenda.repository.Produtos;
+import com.projeto.pedidovenda.repository.filter.ProdutoFilter;
 
 @Named
 @ViewScoped
 public class PesquisaProdutosBean implements Serializable {
 
-        private static final long serialVersionUID = 1L;
-        
-	private List<Integer> produtosFiltrados;
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private Produtos produtos;
+	
+	private ProdutoFilter filtro;
+	private List<Produto> produtosFiltrados;
 	
 	public PesquisaProdutosBean() {
-		produtosFiltrados = new ArrayList<>();
-		for (int i = 0; i < 50; i++) {
-			produtosFiltrados.add(i);
-		}
+		filtro = new ProdutoFilter();
+	}
+	
+	public void pesquisar() {
+		produtosFiltrados = produtos.filtrados(filtro);
+	}
+	
+	public List<Produto> getProdutosFiltrados() {
+		return produtosFiltrados;
 	}
 
-	public List<Integer> getProdutosFiltrados() {
-		return produtosFiltrados;
+	public ProdutoFilter getFiltro() {
+		return filtro;
 	}
 	
 }

73b2aca6e5e6d2c64f0571ce82057baf1b460553
AlexMCoder <alexmpprog@gmail.com>
2016-12-11 19:59:00 -0200

    Implementando a pesquisa de pedidos

--- PedidoVenda/src/main/java/com/projeto/pedidovenda/controller/PesquisaPedidosBean.java
+++ PedidoVenda/src/main/java/com/projeto/pedidovenda/controller/PesquisaPedidosBean.java
@@ -10,25 +10,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import com.projeto.pedidovenda.model.Pedido;
+import com.projeto.pedidovenda.model.StatusPedido;
+import com.projeto.pedidovenda.repository.Pedidos;
+import com.projeto.pedidovenda.repository.filter.PedidoFilter;
 
 @Named
 @ViewScoped
-public class PesquisaPedidosBean implements Serializable{
-        
-        private static final long serialVersionUID = 1L;
-        
-	private List<Integer> pedidosFiltrados;
+public class PesquisaPedidosBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private Pedidos pedidos;
+	
+	private PedidoFilter filtro;
+	private List<Pedido> pedidosFiltrados;
 	
 	public PesquisaPedidosBean() {
+		filtro = new PedidoFilter();
 		pedidosFiltrados = new ArrayList<>();
-		for (int i = 0; i < 50; i++) {
-			pedidosFiltrados.add(i);
-		}
 	}
 
-	public List<Integer> getPedidosFiltrados() {
+	public void pesquisar() {
+		pedidosFiltrados = pedidos.filtrados(filtro);
+	}
+	
+	public StatusPedido[] getStatuses() {
+		return StatusPedido.values();
+	}
+	
+	public List<Pedido> getPedidosFiltrados() {
 		return pedidosFiltrados;
+	}
+
+	public PedidoFilter getFiltro() {
+		return filtro;
 	}
 	
 }
--- PedidoVenda/src/main/java/com/projeto/pedidovenda/model/StatusPedido.java
+++ PedidoVenda/src/main/java/com/projeto/pedidovenda/model/StatusPedido.java
@@ -2,6 +2,15 @@ package com.projeto.pedidovenda.model;
 
 public enum StatusPedido {
 
-	ORCAMENTO, EMITIDO, CANCELADO
-	
+	ORCAMENTO("Orçamento"), EMITIDO("Emitido"), CANCELADO("Cancelado");
+
+	private String descricao;
+
+	StatusPedido(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
 }

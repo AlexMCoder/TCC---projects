8192bac09b842aaee17b9bf0cc68c81ff127981a
AlexMCoder <alexmpprog@gmail.com>
2016-12-22 10:09:41 -0200

    Implementando a edição de pedidos

--- PedidoVenda/src/main/java/com/projeto/pedidovenda/controller/CadastroPedidoBean.java
+++ PedidoVenda/src/main/java/com/projeto/pedidovenda/controller/CadastroPedidoBean.java
@@ -36,41 +36,41 @@ public class CadastroPedidoBean implements Serializable {
 
 	@Inject
 	private Usuarios usuarios;
-
+	
 	@Inject
 	private Clientes clientes;
-
+	
 	@Inject
 	private CadastroPedidoService cadastroPedidoService;
-
+	
 	private Pedido pedido;
 	private List<Usuario> vendedores;
-
+	
 	public CadastroPedidoBean() {
 		limpar();
 	}
-
+	
 	public void inicializar() {
 		if (FacesUtil.isNotPostback()) {
 			this.vendedores = this.usuarios.vendedores();
 		}
 	}
-
+	
 	private void limpar() {
 		pedido = new Pedido();
 		pedido.setEnderecoEntrega(new EnderecoEntrega());
 	}
-
+	
 	public void salvar() {
 		this.pedido = this.cadastroPedidoService.salvar(this.pedido);
-
+		
 		FacesUtil.addInfoMessage("Pedido salvo com sucesso!");
 	}
-
+	
 	public FormaPagamento[] getFormasPagamento() {
 		return FormaPagamento.values();
 	}
-
+	
 	public List<Cliente> completarCliente(String nome) {
 		return this.clientes.porNome(nome);
 	}
@@ -78,9 +78,17 @@ public class CadastroPedidoBean implements Serializable {
 	public Pedido getPedido() {
 		return pedido;
 	}
+	
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
 
 	public List<Usuario> getVendedores() {
 		return vendedores;
+	}
+	
+	public boolean isEditando() {
+		return this.pedido.getId() != null;
 	}
 
 }

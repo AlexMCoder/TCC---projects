7a1d0f039282a58e6701012ed098e85c7e3ec379
AlexMCoder <alexmpprog@gmail.com>
2017-01-04 19:19:49 -0200

    Template de e-mail com Apache Velocity

--- PedidoVenda/src/main/java/com/projeto/pedidovenda/controller/EnvioPedidoEmailBean.java
+++ PedidoVenda/src/main/java/com/projeto/pedidovenda/controller/EnvioPedidoEmailBean.java
@@ -1,15 +1,19 @@
 package com.projeto.pedidovenda.controller;
 
 import java.io.Serializable;
+import java.util.Locale;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.apache.velocity.tools.generic.NumberTool;
+
 import com.projeto.pedidovenda.model.Pedido;
 import com.projeto.util.jsf.FacesUtil;
 import com.projeto.util.mail.Mailer;
 import com.outjected.email.api.MailMessage;
+import com.outjected.email.impl.templating.velocity.VelocityTemplate;
 
 @Named
 @RequestScoped
@@ -19,18 +23,24 @@ public class EnvioPedidoEmailBean implements Serializable {
 
 	@Inject
 	private Mailer mailer;
-
+	
 	@Inject
 	@PedidoEdicao
 	private Pedido pedido;
-
+	
 	public void enviarPedido() {
 		MailMessage message = mailer.novaMensagem();
-
-		message.to(this.pedido.getCliente().getEmail()).subject("Pedido " + this.pedido.getId())
-				.bodyHtml("<strong>Valor total:</strong> " + this.pedido.getValorTotal()).send();
-
+		
+		message.to(this.pedido.getCliente().getEmail())
+			.subject("Pedido " + this.pedido.getId())
+			.bodyHtml(new VelocityTemplate(getClass().getResourceAsStream("/emails/pedido.template")))
+			.put("pedido", this.pedido)
+			.put("numberTool", new NumberTool())
+			.put("locale", new Locale("pt", "BR"))
+			.send();
+		
 		FacesUtil.addInfoMessage("Pedido enviado por e-mail com sucesso!");
 	}
-
+	
+	
 }
